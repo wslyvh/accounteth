4d02b5826a30118938e45e357f02902c39499189
wslyvh <wslyvh@users.noreply.github.com>
2020-05-04 22:24:13 +0200

    cra

---
+++ src/pages/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../assets/App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <h1>Account-eth</h1>
+      </header>
+      <div>
+        ...
+      </div>
+    </div>
+  );
+}
+
+export default App;
